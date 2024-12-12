b9e5581f327561b4e835ea715a0c56e288bdd148
CashMoneyMichael M <mmikey1010@gmail.com>
2024-12-12 19:47:20 +0000

    madlib story

---
+++ finalproject.py
@@ -0,0 +1,7 @@
+#madlib story:
+#Yesterday was a (adjective) day. I started the day by eating (breakfast food) but it tasted weird and
+#smelled like (a type of fish). After that, I went to the (place) to ease my mind, but then a (adjective) man with a 
+#(object) came in and started (verb that ends in -ing). I started to get bored so I decided to go to the (place) to 
+#(verb). However, I got interrupted by a (animal) that started to (verb). It was a very (adjective) animal but I just
+# wasn't in the mood. By then, I was feeling so (adjective) that I decided I should go to my room at home and take a nap.
+# I was so (adjective) and ready to rest, after my long (adjective) day.
