99ecfd5b22ed5b98b31a91ca8594a23f8d47bb28
CashMoneyMichael M <mmikey1010@gmail.com>
2024-12-16 23:22:39 +0000

    test

--- finalproject.py
+++ finalproject.py
@@ -4,4 +4,4 @@
 #(object) came in and started (verb that ends in -ing). I started to get bored so I decided to go to the (place) to 
 #(verb). However, I got interrupted by a (animal) that started to (verb). It was a very (adjective) animal but I just
 # wasn't in the mood. By then, I was feeling so (adjective) that I decided I should go to my room at home and take a nap.
-# I was so (adjective) and ready to rest, after my long (adjective) day.
+# I was so (adjective) and ready to rest, after my long (adjective) day. OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
