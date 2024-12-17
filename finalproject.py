cd8d87258456a36879727ba679111a10aaa49b7c
CashMoneyMichael M <mmikey1010@gmail.com>
2024-12-17 01:51:17 +0000

    finished project

--- finalproject.py
+++ finalproject.py
@@ -26,16 +26,14 @@ def madlib_blanks():
         "verb2": get_user_input("verb"),
         "animal": get_user_input("animal")
     }
-    print(f"""
-    Yesterday was a {blanks["adjective1"]} day. I started the day by eating {blanks["breakfast food"]}, 
+    print(f"""Yesterday was a {blanks["adjective1"]} day. I started the day by eating {blanks["breakfast food"]}, 
     but it tasted weird and smelled like {blanks["a type of fish"]}. After that, I went to the 
     {blanks["place1"]} to ease my mind, but then a {blanks["adjective2"]} man with a {blanks["object"]} 
     came in and started {blanks["verb (-ing)"]}. I started to get bored, so I decided to go to the 
     {blanks["place2"]} to {blanks["verb1"]}. However, I got interrupted by a {blanks["animal"]} 
     that started to {blanks["verb2"]}. It was a very {blanks["adjective3"]} animal, but I just wasn't in the mood. 
     By then, I was feeling so {blanks["adjective4"]} that I decided I should go to my room at home and take a nap. 
-    I was so {blanks["adjective5"]} and ready to rest, after my long {blanks["adjective1"]} day.
-    """)
+    I was so {blanks["adjective5"]} and ready to rest, after my long {blanks["adjective1"]} day.""")
 
 
 madlib_blanks()
